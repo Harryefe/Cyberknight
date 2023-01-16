01d5a896d74565b4e6b1b5363eeaa4f5a11567f7
Harryefe <efeharry@gmail.com>
2023-01-16 10:41:12 +0000

    Changes Have been made on home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,12 +13,14 @@
           class="col-xl-6 col-md-6 pt-lg-5 text-center text-md-start mb-4 mb-md-0"
         >
           <h1 class="display-3 pb-2 pb-sm-3 text-white">
-            Connect easily with<br />Colleagues and Team Members
+            Connect easily with<br />friends and team
           </h1>
           <p
             class="fs-lg d-md-none d-xl-block pb-2 pb-md-0 mb-4 mb-md-5 fw-bold-"
           >
-            On the renowned CyberKnight platform, you can bring colleagues together, revamp workspaces, attract new audiences, and thrill your friends..
+            Bring teams together, redesign workspaces, engage new audiences,
+            and delight your friends — all on the CyberKnight platform you know and
+            love.
           </p>
           <form class="d-none d-sm-flex mb-5 w-60">
             <div class="input-group d-block d-sm-flex input-group-lg me-3">
@@ -74,7 +76,7 @@
               </div>
             </div>
             <div class="text-light">
-              <strong>600</strong> users already with us
+              <strong>600k+</strong> users already with us
             </div>
           </div>
         </div>
